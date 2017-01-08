5951375f4be2fa54b3fffc1f6490965d9e96351f
simonlikesfartingerasers <simonlikesfartingerasers@gmail.com>
2017-01-08 21:50:51 +0900

    Experimentation
    I was just playing around with Python because I was just a beginner
    that just started to learn Python about 30 minutes ago.

---
+++ script.py
@@ -0,0 +1,7 @@
+catrina = "Hi, my name is Catrina!"
+hugh = "And my name is Hugh"
+sarah = "I'm Sarah!"
+emma = "I'm Emma!"
+joe = "And I'm Joe to you!"
+
+print catrina + " " + hugh + " " + sarah + " " + emma + " " + joe
